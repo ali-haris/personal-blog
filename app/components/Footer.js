d3677e8544c033e9f28ae42154e948360fe553e8
yarshy38 <107096990+yarshy38@users.noreply.github.com>
2023-06-15 15:46:37 +0500

    updated code

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { ImFacebook, ImLinkedin2, ImGithub } from "react-icons/im";
+import Link from 'next/link';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <>
+            <section className='bg-gray-50'>
+                <div className='container mx-auto flex justify-center py-12'>
+                    <div className='py-5'>
+                        <div className='flex gap-6 justify-center'>
+                            <Link href={'/'}><ImFacebook color='#888888' /> </Link>
+                            <Link href={'/'}> <ImGithub color='#888888' /> </Link>
+                            <Link href={'/'}>   <ImLinkedin2 color='#888888' /> </Link>
+                        </div>
+                        <p className='py-5 text-gray-400'>Copyright 2023 All right reserved || hello there</p>
+                        <p className='text-gray-400 text-center'>Terms & Conditions</p>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
---
+++ app/components/_child/Newsletter.js
@@ -0,0 +1,14 @@
+import Section1 from "../Section1"
+
+export default function Newsletter() {
+    return (
+        <>
+            <section className='bg-gray-500 mt-20'>
+
+                <div className="container mx-auto md:px-20 py-12 text-center">
+                    <h1 className="font-bold text-3xl">Newsletter</h1>
+                </div>
+            </section>
+        </>
+    )
+}
