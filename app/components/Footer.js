1a2e45b5f9dd0e83529cec182304064cb6823574
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2023-07-26 15:44:11 +0500

    updated

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -13,10 +13,12 @@ function Footer() {
                         <div className='flex gap-6 justify-center'>
                             <Link href={'/'}><ImFacebook color='#888888' /> </Link>
                             <Link href={'/'}> <ImGithub color='#888888' /> </Link>
+                            <Link href={'/'}> <ImGithub color='#888888' /> </Link>
                             <Link href={'/'}>   <ImLinkedin2 color='#888888' /> </Link>
                         </div>
                         <p className='py-5 text-gray-400'>Copyright 2023 All right reserved || hello there</p>
                         <p className='text-gray-400 text-center'>Terms & Conditions</p>
+                        <p className='text-gray-400 text-center'>Terms </p>
                     </div>
                 </div>
             </footer>
